d2b93615d415d7a7e0b56be9154121bc547634e9
Pranav Suthar <pranavsuthar2311@gmail.com>
2024-10-21 12:22:45 +0530

    [2024-10-21 12:22:44] Variables Scope

---
+++ S02_PH01_Datatype.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+// Integer
+// $age = 25;
+// echo $age; // Output: 25
+// //---------------------------------------------
+
+
+// // Boolean
+// $isLoggedIn = true;
+// echo $isLoggedIn;
+// //---------------------------------------------
+
+
+// // FLoat / Double
+// $price = 19.99;
+// echo $price; // Output: 19.99
+// //---------------------------------------------
+
+
+// // String
+// $name = "John Doe";
+// echo $name; // Output: John Doe
+// //---------------------------------------------
+
+
+// // Array is s collaction of same datatype
+// $fruits = array("apple", "banana", "orange");
+// echo $fruits[1];
+//---------------------------------------------
+
+
+// Null
+// $var = null;
+// echo "This is NUll : ". $var; // Output: (nothing)
+//------------------------------------------
+
+// Escape Sec
+// echo "Hello\tWorld!";
+
+// Functions
+// function echHello(){
+//     echo "Hello from Function";
+// }
+
+// echHello() ;
+
+
+// Global Scope Vriables
+// $a = 5;
+// function myName()
+// {
+//     global $a;
+//    echo "Inside Function $a <br>";
+// }
+
+// myName();
+
+// echo "Globale $a"
+
+?>
+
+</body>
+</html>
