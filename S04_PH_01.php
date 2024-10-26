6e3767537f19d4efc78965d7f6663347d818570b
Pranav Suthar <pranavsuthar2311@gmail.com>
2024-10-26 17:14:22 +0530

    [2024-10-26 17:14:20] If Else ladder, Switch case, For loop

---
+++ S04_PH_01.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>
+        <?php
+
+        // $age = 17;
+        // if ($age >= 18) {
+        //     echo "You can vote";
+        // } 
+        // else {
+        //     echo "You can not vote";
+        // }
+
+
+        // if else ladder
+        // $city =  "Lahore";
+
+        // if ($city === "Delhi"  ) {
+
+        //     echo "";
+
+        // } else if ($marks  >= 80 && $marks < 90) {
+
+        //     echo "A";
+
+        // } else if ($marks >= 70 && $marks < 80) {
+
+        //     echo "B";
+
+        // } else if ($marks >= 60 && $marks < 70) {
+
+        //     echo "C";
+
+        // } else {
+
+        //     echo "F";
+
+        // }
+
+
+
+        // Switch Case Syntax
+        // switch (variable) {
+        // case value1:
+        // Code for case value1
+        // break;
+        // case value2:
+        // Code for case value2
+        // break;
+        // default:
+        // Code if no case matches
+        // }
+
+        // $day = 50;
+
+        // switch ($day) {
+        //     case $day < 10:
+        //         echo "Start of the week!";
+        //         break;
+        //     case $day > 10:
+        //         echo "End of the work week!";
+        //         break;
+        //     default:
+        //         echo "It's a regular day.";
+        // }
+        // Output: Start of the week!
+
+
+
+
+        ?>
+    </h1>
+</body>
+
+</html>
---
+++ S04_PH_02.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loop</title>
+</head>
+
+<body>
+
+
+    <h1>
+        <?php
+
+        // for (initialization; condition; increment) {
+        // Code to execute
+        // }
+
+        for ($i = 1; $i <= 10; $i++) {
+            echo $i . "<br>";
+        }
+
+        // Print 
+
+
+        ?>
+    </h1>
+
+
+</body>
+
+</html>
