f7fbad613f549b54b8103ccf0e8c7969d47df01b
Pranav Suthar <pranavsuthar2311@gmail.com>
2024-10-28 12:37:08 +0530

    [2024-10-28 12:37:07] Loops - For, while, Do..While, forEach

--- S04_PH_02.php
+++ S04_PH_02.php
@@ -16,12 +16,46 @@
         // for (initialization; condition; increment) {
         // Code to execute
         // }
+        // for ($i = 1; $i <= 10; $i++) {
+        //     echo $i . "<br>";
+        // }
 
-        for ($i = 1; $i <= 10; $i++) {
-            echo $i . "<br>";
+
+
+        // While Loop
+        // $tankIsFull = false;
+        // $gallonsAdded = 0;
+        // while (!$tankIsFull) {
+        //     $gallonsAdded++;
+        //     echo "Added gallon $gallonsAdded to the tank.<br>";
+        //     if ($gallonsAdded >=10 ) {
+        //         $tankIsFull = true;
+        //     }
+        // }
+
+
+
+        // Do..While Look
+        // $i = 6;
+        // do {
+        //     echo "The value of i is: $i<br>";
+        //     $i++;
+        // } while ($i <= 5);
+
+
+        // forEach Loop
+        $friendsEmails = ["alice@example.com", "bob@example.com", "carol@example.com"];
+        foreach ($friendsEmails as $email) {
+            echo "Sending email to: $email<br>";
         }
 
-        // Print 
+        echo "<hr>";
+
+        $empEmails = ["emp@example.com", "emp2@example.com", "emp3@example.com"];
+        foreach ($empEmails as $email) {
+            echo "Sending email to: $email<br>";
+        }
+
 
 
         ?>
